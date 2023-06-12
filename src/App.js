65cd62b012fe403f110b03798d6d6366a85720c2
StoyanBanov <sbanov05@gmail.com>
2023-06-13 00:13:12 +0300

    add home page and route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+
+            {/* Main Content */}
+            <main id="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;

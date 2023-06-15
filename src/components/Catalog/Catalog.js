f5d1700be5eac7d8815a380df4338e0415914733
StoyanBanov <sbanov05@gmail.com>
2023-06-15 16:29:05 +0300

    fix catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -4,11 +4,11 @@ export const Catalog = ({ games }) => {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* Display div: with information about every game (if any) */}
-            {games.map(g => <CatalogGame key={g._id} game={g} />)}
+            {games.length > 0
+                ? games.map(g => <CatalogGame key={g._id} game={g} />)
 
-            {/* Display paragraph: If there is no games */}
-            <h3 className="no-articles">No articles yet</h3>
+                : <h3 className="no-articles">No articles yet</h3>
+            }
         </section>
     )
 }
